5a3e0d676262cbcddd2a9da8d05330eaf0229b26
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-01-25 15:08:16 +0000

    Initialize tasks.json file in Storage class
    Added logic to the Storage class to create and initialize the tasks.json file with an empty list. This ensures the file exists and is ready for use when the application starts.

--- storage.py
+++ storage.py
@@ -1,4 +1,12 @@
 class Storage:
     # Handles saving and loading data
     # This file saves and loads data from JSON.
-    pass
+
+    task_file = '/Users/mehedimostafa/Desktop/PROJECTS/New Projectxx/Momentum/data/tasks.json'
+
+    try:
+        with open(task_file, 'w') as file:
+            file.write('[]')
+    except FileNotFoundError:
+        with open(task_file, 'w') as file:
+            file.write('[]')
