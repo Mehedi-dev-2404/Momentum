575eaded1e2e52c1844ef9c9751f0b77762746bc
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-02-04 17:48:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -104,7 +104,6 @@ def main():
         print("2. View Today's Schedule")
         print("3. Create/Edit User Profile")
         print("4. Exit")
-        print("")
         choice = input("Choose an option: ")
         if choice == '1':
             add_task(storage)
